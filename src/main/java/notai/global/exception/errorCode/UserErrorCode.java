a4f41576d92e7fda8d4ec8d155c7573526cab51e
sanghyuk <ish775800@gmail.com>
2024-09-03 16:44:26 +0900

    feat: User 관련 ErrorCode 추가

--- src/main/java/notai/global/exception/errorCode/UserErrorCode.java
+++ src/main/java/notai/global/exception/errorCode/UserErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum UserErrorCode implements ErrorCode {
 
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저 정보가 존재하지 않습니다")
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저 정보가 존재하지 않습니다"),
+    USER_PASSWORD_NOT_CORRECT(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다")
     ;
 
     private final HttpStatus httpStatus;
