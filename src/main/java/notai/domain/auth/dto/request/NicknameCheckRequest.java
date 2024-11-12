ff96a619410ef06cc04840dd3ed585785ecb4716
sanghyuk <ish775800@gmail.com>
2024-11-12 21:43:41 +0900

    fix: 닉네임 dto 파싱 에러

--- src/main/java/notai/domain/auth/dto/request/NicknameCheckRequest.java
+++ src/main/java/notai/domain/auth/dto/request/NicknameCheckRequest.java
@@ -1,11 +1,13 @@
 package notai.domain.auth.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class NicknameCheckRequest {
 
     @NotBlank(message = "별명은 필수 입력 항목입니다")
