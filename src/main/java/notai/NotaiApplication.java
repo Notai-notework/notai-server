fd544a29df1687b1d95233eafa2e6f0d19695002
sanghyuk <ish775800@gmail.com>
2024-11-19 10:42:01 +0900

    fix: timezone error

--- src/main/java/notai/NotaiApplication.java
+++ src/main/java/notai/NotaiApplication.java
@@ -1,5 +1,7 @@
 package notai;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -8,6 +10,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableJpaAuditing
 public class NotaiApplication {
+
+	@PostConstruct
+	public void setTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotaiApplication.class, args);
