43ecc234e866dcb3524badb1b15d2560932bc7d7
sanghyuk <ish775800@gmail.com>
2024-11-20 01:05:06 +0900

    feat: 문서 삭제 개선

--- src/main/java/notai/domain/document/controller/DocumentController.java
+++ src/main/java/notai/domain/document/controller/DocumentController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -63,10 +64,11 @@ public class DocumentController {
 
     // 문서 삭제
     @DeleteMapping
-    public ResponseEntity<Void> documentRemove(@Valid @RequestBody DocumentRemoveRequest request,
+    public ResponseEntity<Void> documentRemove(@Valid @RequestParam Long documentId,
         @AuthenticationPrincipal CustomUserDetails customUserDetails) {
 
-        documentService.removeDocument(request, customUserDetails.getUser());
+        documentService.removeDocument(new DocumentRemoveRequest(documentId),
+            customUserDetails.getUser());
 
         return ResponseEntity.noContent().build();
     }
--- src/main/java/notai/domain/document/dto/request/DocumentRemoveRequest.java
+++ src/main/java/notai/domain/document/dto/request/DocumentRemoveRequest.java
@@ -1,11 +1,14 @@
 package notai.domain.document.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DocumentRemoveRequest {
 
     @NotNull(message = "문서 id를 입력해주세요")
